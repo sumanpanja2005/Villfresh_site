86115610a0fc8e37ec85490137d4d057a8200d39
suman panja <161006434+sumanpanja2005@users.noreply.github.com>
2026-01-03 18:04:15 +0530

    Update server to listen on all network interfaces and improve log message

--- server/index.js
+++ server/index.js
@@ -65,6 +65,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
